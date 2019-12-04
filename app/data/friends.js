f33b86ec2c64e2e6b1401939f104b05b8f0bbfe3
Khoa Nguyen <kh.nguyen0101@gmail.com>
2019-12-04 14:03:40 -0800

    added correct survey scores for celebs

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,49 +5,49 @@ var friends = [
     name: "Kylie Jenner",
     photo: "https://scstylecaster.files.wordpress.com/2019/08/kylie-jenner-2.jpg",
     type: "celebrity",
-    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    scores: [5, 5, 1, 5, 5, 2, 5, 1, 1, 5]
   },
   {
     routeName: "kanyewest",
     name: "Kanye West",
     photo: "https://pmcdeadline2.files.wordpress.com/2019/11/shutterstock_editorial_10467948bp.jpg?w=681&h=383&crop=1",
     type: "celebrity",
-    scores: [5, 1, 1, 1, 1, 1, 2, 5, 4, 1]
+    scores: [5, 5, 2, 5, 5, 5, 1, 5, 3, 5]
   },
   {
     routeName: "jenniferlopez",
     name: "Jennifer Lopez",
     photo: "https://pmcvariety.files.wordpress.com/2019/04/jennifer-lopez.jpg?w=1000",
     type: "celebrity",
-    scores: [5, 1, 4, 4, 5, 5, 2, 5, 5, 1]
+    scores: [3, 4, 2, 1, 3, 4, 4, 5, 5, 3]
   },
   {
     routeName: "jonahhill",
     name: "Jonah Hill",
     photo: "https://nyoobserver.files.wordpress.com/2019/09/gettyimages-1097034660.jpg?quality=80&w=635&h=423",
     type: "celebrity",
-    scores: [5, 1, 4, 4, 5, 5, 2, 5, 5, 1]
+    scores: [1, 1, 5, 1, 2, 3, 4, 1, 5, 1]
   },
   {
     routeName: "zendaya",
     name: "Zendaya",
     photo: "https://pmcfootwearnews.files.wordpress.com/2019/09/zendaya-01.jpg?w=700&h=437&crop=1",
     type: "celebrity",
-    scores: [5, 1, 4, 4, 5, 5, 2, 5, 5, 1]
+    scores: [2, 3, 5, 1, 3, 2, 5, 2, 5, 1]
   },
   {
     routeName: "trevornoah",
     name: "Trevor Noah",
     photo: "https://cdn1.thr.com/sites/default/files/imagecache/768x433/2019/08/trevor_noah-publicity_still-h_2019.jpg",
     type: "celebrity",
-    scores: [5, 1, 4, 4, 5, 5, 2, 5, 5, 1]
+    scores: [1, 3, 5, 1, 2, 5, 5, 2, 5, 2]
   },
   {
     routeName: "beyonce",
     name: "Beyonce",
     photo: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fewedit.files.wordpress.com%2F2019%2F11%2Fgettyimages-962872295.jpg&w=400&c=sc&poi=face&q=85",
     type: "celebrity",
-    scores: [5, 1, 4, 4, 5, 5, 2, 5, 5, 1]
+    scores: [3, 4, 2, 1, 4, 5, 4, 5, 3, 3]
   }
 ];
 
