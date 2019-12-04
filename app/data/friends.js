148efbea1842b0a5ffbcb968a766148330ae6bc4
Khoa Nguyen <kh.nguyen0101@gmail.com>
2019-12-04 13:58:18 -0800

    added links to celeb photos

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,7 +3,7 @@ var friends = [
   {
     routeName: "kyliejenner",
     name: "Kylie Jenner",
-    photo: "#",
+    photo: "https://scstylecaster.files.wordpress.com/2019/08/kylie-jenner-2.jpg",
     type: "celebrity",
     scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
   },
@@ -17,35 +17,35 @@ var friends = [
   {
     routeName: "jenniferlopez",
     name: "Jennifer Lopez",
-    photo: "#",
+    photo: "https://pmcvariety.files.wordpress.com/2019/04/jennifer-lopez.jpg?w=1000",
     type: "celebrity",
     scores: [5, 1, 4, 4, 5, 5, 2, 5, 5, 1]
   },
   {
     routeName: "jonahhill",
     name: "Jonah Hill",
-    photo: "#",
+    photo: "https://nyoobserver.files.wordpress.com/2019/09/gettyimages-1097034660.jpg?quality=80&w=635&h=423",
     type: "celebrity",
     scores: [5, 1, 4, 4, 5, 5, 2, 5, 5, 1]
   },
   {
     routeName: "zendaya",
     name: "Zendaya",
-    photo: "#",
+    photo: "https://pmcfootwearnews.files.wordpress.com/2019/09/zendaya-01.jpg?w=700&h=437&crop=1",
     type: "celebrity",
     scores: [5, 1, 4, 4, 5, 5, 2, 5, 5, 1]
   },
   {
     routeName: "trevornoah",
     name: "Trevor Noah",
-    photo: "#",
+    photo: "https://cdn1.thr.com/sites/default/files/imagecache/768x433/2019/08/trevor_noah-publicity_still-h_2019.jpg",
     type: "celebrity",
     scores: [5, 1, 4, 4, 5, 5, 2, 5, 5, 1]
   },
   {
     routeName: "beyonce",
     name: "Beyonce",
-    photo: "#",
+    photo: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fewedit.files.wordpress.com%2F2019%2F11%2Fgettyimages-962872295.jpg&w=400&c=sc&poi=face&q=85",
     type: "celebrity",
     scores: [5, 1, 4, 4, 5, 5, 2, 5, 5, 1]
   }
